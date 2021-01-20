77d208355abc14db11dd32f57304be56144fca80
mariarybakonenko <73584314+mariarybakonenko@users.noreply.github.com>
2021-01-20 23:27:59 +0300

    finish
    -add responsive design
    -add styles for 4 ranges
    -finish project

--- main.js
+++ main.js
@@ -10,12 +10,20 @@ let imgs = document.querySelectorAll('.inst__slider_list_item_img');
 let titles = document.querySelectorAll('.inst__slider_list_item_name');
 let main = document.querySelector('.inst__content_img');
 
+let slider_list = document.querySelector('.inst__slider_list_wrapper');
+let slider_range = document.querySelector('.inst__slider_range');
+
+let btn_r = document.querySelector('.btn-right');
+let btn_l = document.querySelector('.btn-left');
+
 let main_wrap = document.querySelector('.inst__content');
 main_wrap.style.filter = 'contrast(100%)'
 
+let array = document.querySelectorAll('.inst__slider_range_input');
+let values = document.querySelectorAll('.inst__slider_range_input_value');
+
 
 for (let i = 0; i < items.length; i++) {
-
     items[i].addEventListener('click', function () {
         Filter(i);
     }, false);
@@ -27,36 +35,73 @@ btn_rotate.addEventListener('click', function () {
     current_rotation -= 90;
     if (current_rotation <= -355) {
         current_rotation = 0;
-    } else{
+    } else {
         current_rotation;
     }
     main.style.transform = `rotateZ(${current_rotation}deg)`;
 
-        }, false);
-
+}, false);
 
 let btn_flip = document.querySelector('.flip')
 btn_flip.addEventListener('click', function () {
     current_flip = current_flip * (-1);
-    main.style.transform = `scale(${current_flip},1)`
+    main_wrap.style.transform = `scale(${current_flip},1)`
 }, false);
-
 
 let btn_scale = document.querySelector('.scale')
 btn_scale.addEventListener('click', function () {
     let n = parseFloat(window.getComputedStyle(main_wrap).filter.split('(')[1])
+    btn_scale.className = 'inst__nav_social scale';
 
-    if (n === 1 ) {
-        main_wrap.style.filter = 'contrast(140%)'
-    }
-    else if (n > 1) {
+    if (n === 1) {
+        main_wrap.style.filter = 'contrast(140%)';
+        btn_scale.classList.add('scale-contrast')
+    } else if (n > 1) {
         main_wrap.style.filter = 'contrast(60%)'
-    }
-    else if (n < 1) {
+        btn_scale.classList.add('scale-low-contrast')
+    } else if (n < 1) {
         main_wrap.style.filter = 'contrast(100%)'
     }
 
 }, false);
+
+let btn_logo = document.querySelector('.logo')
+btn_logo.addEventListener('click', function () {
+    current_rotation = 0;
+    current_flip = 1;
+    main_wrap.style.filter = 'contrast(100%)';
+    main.style.transform = `rotateZ(${current_rotation}deg)`;
+    main_wrap.style.transform = `scale(${current_flip},1)`;
+    main.className = 'inst__content_img'
+    for (let i = 0; i < array.length; i++) {
+        let n
+        if (i === 0) {
+            n = 100;
+            main.style.width = n + '%';
+        } else if (i === 1) {
+            n = 100;
+            main.style.height = n + '%';
+        } else if (i === 2) {
+            n = 0;
+            main.style.right = n + 'px';
+        } else if (i === 3) {
+            n = 0;
+            main.style.bottom = n + 'px';
+        }
+        ;
+        array[i].value = n;
+        let value_n = values[i];
+        value_n.innerHTML = n;
+    }
+
+}, false);
+
+let btn_crop = document.querySelector('.fill')
+btn_crop.addEventListener('click', function () {
+    Crop();
+}, false);
+
+
 
 function Slider(p) {
 
@@ -66,8 +111,6 @@ function Slider(p) {
         let n = parseInt(window.getComputedStyle(items[i]).left);
         items[i].style.transition = '500ms';
 
-        let btn_r = document.querySelector('.btn-right');
-        let btn_l = document.querySelector('.btn-left');
         btn_r.classList.remove('disabled');
         btn_l.classList.remove('disabled');
         if (p === 'right') {
@@ -109,4 +152,40 @@ function Filter(n) {
     ;
     titles[n].classList.add('active')
 };
+
+function Crop() {
+    slider_list.classList.toggle('hidden');
+    slider_list.classList.toggle('visible');
+    slider_range.classList.toggle('hidden_none');
+    slider_range.classList.toggle('visible');
+    btn_l.classList.toggle('hidden_none');
+    btn_l.classList.toggle('visible');
+    btn_r.classList.toggle('hidden_none');
+    btn_r.classList.toggle('visible');
+}
+
+function Values() {
+    for (let i = 0; i < array.length; i++) {
+        array[i].addEventListener('mousemove', function () {
+            let n = parseInt(array[i].value)
+            let value_n = values[i]
+            value_n.innerHTML = n
+            if (i === 0) {
+                main.style.width = n + '%'
+            } else if (i === 1) {
+                main.style.height = n + '%'
+            } else if (i === 2) {
+                main.style.right = n + 'px'
+            } else if (i === 3) {
+                main.style.bottom = n + 'px'
+            }
+            ;
+        })
+    }
+    ;
+}
+
+
+
+Values();
 
